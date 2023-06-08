58a31d7c93907510452af0e4e4211f453fc232d7
foridislam332 <foridislam991@gmail.com>
2023-06-08 14:37:48 +0600

    sign up page done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,26 +7,35 @@ import { Link } from 'react-router-dom';
 // react icons
 import { MdAlternateEmail, MdLockOutline } from 'react-icons/md';
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
+import { HiOutlinePhotograph } from 'react-icons/hi';
+import { BiUserPin } from 'react-icons/bi';
+import { BsShieldCheck } from 'react-icons/bs';
 
 // image
 import googleImg from '../assets/images/google.png';
 
-const Login = () => {
+const SignUp = () => {
     const [type, setType] = useState('password');
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data)
+        if (data.password.length < 6) {
+            return alert('pass should be 6 cherecter')
+        }
+        if (data.password !== data.confirm) {
+            return alert('pass did not match')
+        }
+        return console.log(data)
     };
     return (
         <section>
-            <Breadcrumbs title="Login" />
+            <Breadcrumbs title="Sign Up" />
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
-                <div className='grid md:grid-cols-2 gap-10 place-items-center'>
+                <div className='grid lg:grid-cols-2 gap-10 place-items-center'>
                     <div>
                         <Player
                             autoplay
                             loop
-                            src="https://assets1.lottiefiles.com/packages/lf20_mjlh3hcy.json"
+                            src="https://assets8.lottiefiles.com/packages/lf20_kdCeeh2u4M.json"
                             className='w-full'
                         >
                         </Player>
@@ -34,17 +43,21 @@ const Login = () => {
 
                     <div className='p-5 md:p-16 rounded-lg shadow-custom max-w-xl mx-auto'>
                         <h1 className='text-3xl md:text-5xl font-semibold text-dark'>Welcome Back!</h1>
-                        <h2 className='text-lg text-gray mb-10'>Login to continue</h2>
-
+                        <h2 className='text-lg text-gray mb-10'>Sign Up to continue</h2>
 
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className='flex items-center border border-green rounded-lg gap-3 p-3'>
+                                <label htmlFor="name"> <BiUserPin className='text-green text-2xl' /></label>
+                                <input className='w-full border-none outline-none' id='name' placeholder="Enter full name" {...register("name")} />
+                            </div>
+
+                            <div className='flex items-center border border-green rounded-lg gap-3 p-3 mt-3'>
                                 <label htmlFor="email"> <MdAlternateEmail className='text-green text-2xl' /></label>
-                                <input className='w-full border-none outline-none' id='email' placeholder="Enter email" {...register("email", { required: true })} />
+                                <input type='email' className='w-full border-none outline-none' id='email' placeholder="Enter your email" {...register("email", { required: true })} />
                             </div>
                             {errors.email && <span className='text-sm text-red ml-1'>Email is required</span>}
 
-                            <div className='flex items-center border border-green rounded-lg gap-3 p-3 mt-5'>
+                            <div className='flex items-center border border-green rounded-lg gap-3 p-3 mt-3'>
                                 <label htmlFor="password"> <MdLockOutline className='text-green text-2xl' /></label>
                                 <input type={type} className='w-full border-none outline-none' id='password' placeholder="password" {...register("password", { required: true })} />
                                 <button onClick={() => setType(type === 'password' ? 'text' : 'password')}>
@@ -54,9 +67,24 @@ const Login = () => {
                                 </button>
                             </div>
                             {errors.password && <span className='text-sm text-red ml-1'>Password is required</span>}
-                            <Link to='/login' className='text-sm text-right  text-gray block'>Forget password?</Link>
 
-                            <button className='btn_primary w-full mt-7 border' type='submit'>Login</button>
+                            <div className='flex items-center border border-green rounded-lg gap-3 p-3 mt-3'>
+                                <label htmlFor="confirm"> <BsShieldCheck className='text-green text-2xl' /></label>
+                                <input type={type} className='w-full border-none outline-none' id='confirm' placeholder="Confirm password" {...register("confirm", { required: true })} />
+                                <button onClick={() => setType(type === 'password' ? 'text' : 'password')}>
+                                    {
+                                        type === 'password' ? <AiOutlineEyeInvisible /> : <AiOutlineEye />
+                                    }
+                                </button>
+                            </div>
+                            {errors.password && <span className='text-sm text-red ml-1'>Confirm password is required</span>}
+
+                            <div className='flex items-center border border-green rounded-lg gap-3 p-3 mt-3'>
+                                <label htmlFor="photo"> <HiOutlinePhotograph className='text-green text-2xl' /></label>
+                                <input className='w-full border-none outline-none' id='photo' placeholder="Photo URL" {...register("photo")} />
+                            </div>
+
+                            <button className='btn_primary w-full mt-7 border' type='submit'>Sign Up</button>
                         </form>
 
                         <div className='flex items-center gap-3 mx-5 my-5'>
@@ -70,7 +98,7 @@ const Login = () => {
                             <p className='text-lg'>Login with Google</p>
                         </button>
 
-                        <p className='mt-5 text-center text-gray'>New to Eco Learner? <Link className='text-green' to='/signUp'>Sign Up</Link></p>
+                        <p className='mt-5 text-center text-gray'>Already have an account? <Link className='text-green' to='/login'>Login</Link></p>
                     </div>
                 </div>
             </div>
@@ -78,4 +106,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 
 const routes = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const routes = createBrowserRouter([
             {
                 path: 'login',
                 element: <Login />
+            },
+            {
+                path: 'signUp',
+                element: <SignUp />
             }
         ]
     },
